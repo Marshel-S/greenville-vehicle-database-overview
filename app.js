1d55550ded1ca506b905ec650fd738a2c63643ba
Marshel-S <kace93126@gmail.com>
2026-04-09 16:22:17 +0700

    Building express and routes, for localhost network

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "homepage.html"));
+});
+
+app.get("/vehicle", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "database.html"));
+});
+
+app.get("/job", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "jobs.html"));
+});
+
+app.get("/wiki", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "wiki.html"));
+});
+
+app.listen(3000, () => {
+  console.log('Express server running on http://localhost:3000');
+});
