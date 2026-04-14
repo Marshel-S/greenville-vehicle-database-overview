1fbede72639ee7d47f64545ab59ab4d5338f1075
Marshel-S <kace93126@gmail.com>
2026-04-14 11:04:04 +0700

    Vehicle List: Name, type, category ALL

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const path = require('path');
+const { PrismaClient } = require("@prisma/client");
+const express = require("express");
+const path = require("path");
+
 const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "homepage.html"));
+});
 
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "homepage.html"));
@@ -24,6 +34,11 @@ app.get("/wiki", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "wiki.html"));
 });
 
+app.get("/api/vehicles", async (req, res) => {
+  const vehicles = await prisma.vehicle.findMany();
+  res.json(vehicles);
+});
+
 app.listen(3000, () => {
-  console.log('Express server running on http://localhost:3000');
+  console.log("http://localhost:3000");
 });
---
+++ prisma/migrations/20260414032659_fixed/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `car` ADD COLUMN `price` INTEGER NULL;
